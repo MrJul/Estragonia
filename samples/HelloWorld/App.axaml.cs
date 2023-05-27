fbcb176ffd1979dad6328e656b68fa4b135652a0
Julien Lebosquain <julien@lebosquain.net>
2023-05-27 16:08:43 +0200

    Style fixes

--- samples/HelloWorld/App.axaml.cs
+++ samples/HelloWorld/App.axaml.cs
@@ -3,7 +3,7 @@ using Avalonia.Markup.Xaml;
 
 namespace HelloWorld;
 
-public partial class App : Application {
+public class App : Application {
 
 	public override void Initialize()
 		=> AvaloniaXamlLoader.Load(this);
