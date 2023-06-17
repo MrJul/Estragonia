4f18e2ac162b9e29ad89ce9fb1f51d84a317dddc
Julien Lebosquain <julien@lebosquain.net>
2023-06-17 11:02:53 +0200

    Updated Avalonia to latest nightly

--- samples/HelloWorld/HelloWorldView.axaml.cs
+++ samples/HelloWorld/HelloWorldView.axaml.cs
@@ -1,14 +1,11 @@
 ﻿using Avalonia.Controls;
-using Avalonia.Media;
 
 namespace HelloWorld;
 
 public partial class HelloWorldView : UserControl {
 
-	public HelloWorldView() {
-		_ = new SolidColorBrush(); // force the color brush animator to be registered
-		InitializeComponent();
-	}
+	public HelloWorldView()
+		=> InitializeComponent();
 
 }
 
