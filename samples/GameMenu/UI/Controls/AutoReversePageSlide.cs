2006c87116a4e92d9957cb8f312dee548fab7244
Julien Lebosquain <julien@lebosquain.net>
2023-06-21 12:59:22 +0200

    Game menu sample: transition between views

---
+++ samples/GameMenu/UI/Controls/AutoReversePageSlide.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Animation;
+
+namespace GameMenu.UI.Controls;
+
+/// <summary>A slide transition that goes backwards when coming back to the main menu.</summary>
+public sealed class AutoReversePageSlide : PageSlide {
+
+	public override Task Start(Visual? from, Visual? to, bool forward, CancellationToken cancellationToken) {
+		if (to?.DataContext is MainMenuViewModel)
+			forward = !forward;
+
+		return base.Start(from, to, forward, cancellationToken);
+	}
+
+}
