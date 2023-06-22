7574a8bdc932c1b623f3aab6ae95b155fc66a2eb
Julien Lebosquain <julien@lebosquain.net>
2023-06-22 12:22:52 +0200

    Game menu sample: disable hit test on transition

--- samples/GameMenu/UI/Controls/AutoReversePageSlide.cs
+++ samples/GameMenu/UI/Controls/AutoReversePageSlide.cs
@@ -2,17 +2,29 @@
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Animation;
+using Avalonia.Input;
 
 namespace GameMenu.UI.Controls;
 
 /// <summary>A slide transition that goes backwards when coming back to the main menu.</summary>
 public sealed class AutoReversePageSlide : PageSlide {
 
-	public override Task Start(Visual? from, Visual? to, bool forward, CancellationToken cancellationToken) {
+	public override async Task Start(Visual? from, Visual? to, bool forward, CancellationToken cancellationToken) {
+		var fromElement = from as InputElement;
+
+		if (fromElement is not null)
+			fromElement.IsHitTestVisible = false;
+
 		if (to?.DataContext is MainMenuViewModel)
 			forward = !forward;
 
-		return base.Start(from, to, forward, cancellationToken);
+		try {
+			await base.Start(from, to, forward, cancellationToken);
+		}
+		finally {
+			if (fromElement is not null)
+				fromElement.IsHitTestVisible = true;
+		}
 	}
 
 }
