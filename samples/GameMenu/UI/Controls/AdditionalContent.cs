f76feed8231072e1c0f5edda49598d0c30e45ee1
Julien Lebosquain <julien@lebosquain.net>
2023-06-30 12:31:01 +0200

    Game menu sample: added button icons

---
+++ samples/GameMenu/UI/Controls/AdditionalContent.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace GameMenu.UI.Controls;
+
+public static class AdditionalContent {
+
+	public static readonly AttachedProperty<Geometry?> GeometryProperty =
+		AvaloniaProperty.RegisterAttached<Control, Geometry?>("Geometry", typeof(AdditionalContent));
+
+	public static void SetGeometry(Control obj, Geometry? value)
+		=> obj.SetValue(GeometryProperty, value);
+
+	public static Geometry? GetGeometry(Control obj)
+		=> obj.GetValue(GeometryProperty);
+
+}
