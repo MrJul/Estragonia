a442f5b487cc910fd0f776eac8250173d53a9be3
Julien Lebosquain <julien@lebosquain.net>
2023-07-03 10:24:47 +0200

    Samples: designer support without extra project

--- samples/GameMenu/UI/AsyncGodotResourceLoader.cs
+++ samples/GameMenu/UI/AsyncGodotResourceLoader.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 using Godot;
 using GdArray = Godot.Collections.Array;
---
+++ samples/HelloWorld/Designer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Avalonia;
+
+namespace HelloWorld;
+
+internal static class Designer {
+
+	public static int Main()
+		=> throw new NotSupportedException("This project isn't meant to be run: it's only for Avalonia designer support.");
+
+	// Used by designer
+	public static AppBuilder BuildAvaloniaApp()
+		=> AppBuilder
+			.Configure<App>()
+			.UseSkia();
+
+}
